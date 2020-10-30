5aeb0d614e7a248f716f2f46c85b1be1ef74ad95
wry <72523401+worryy@users.noreply.github.com>
2020-10-30 11:22:26 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// create a new scene
+let gameScene = new Phaser.Scene('Game');
+
+// some parameters for our scene
+gameScene.init = function() {};
+
+// load asset files for our game
+gameScene.preload = function() {
+
+  // load images
+  this.load.image('ground', 'assets/images/ground.png');
+  this.load.image('platform', 'assets/images/platform.png');
+  this.load.image('block', 'assets/images/block.png');
+  this.load.image('goal', 'assets/images/gorilla3.png');
+  this.load.image('barrel', 'assets/images/barrel.png');
+
+  // load spritesheets
+  this.load.spritesheet('player', 'assets/images/player_spritesheet.png', {
+    frameWidth: 28,
+    frameHeight: 30,
+    margin: 1,
+    spacing: 1
+  });
+
+  this.load.spritesheet('fire', 'assets/images/fire_spritesheet.png', {
+    frameWidth: 20,
+    frameHeight: 21,
+    margin: 1,
+    spacing: 1
+  });
+};
+
+// executed once, after assets were loaded
+gameScene.create = function() {
+
+  // 1) adding existing sprites to the physics system
+  // sprite creation
+  let ground = this.add.sprite(180, 400, 'ground');
+
+  // add sprite to the physics system
+  this.physics.add.existing(ground);
+
+  // 2) creating and adding sprites to the physics system
+  let ground2 = this.physics.add.sprite(180, 200, 'ground');
+
+  console.log(ground2);
+};
+
+// our game's configuration
+let config = {
+  type: Phaser.AUTO,
+  width: 360,
+  height: 640,
+  scene: gameScene,
+  title: 'Monster Kong',
+  pixelArt: false,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {y: 1000},
+      debug: true
+    }
+  }
+};
+
+// create the game, and pass it the configuration
+let game = new Phaser.Game(config);
